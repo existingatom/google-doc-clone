f6c3f641498f441f3908cdfe30ff3154939a84df
aditi <aditi.khataniar@gmail.com>
2024-07-17 12:04:46 +0530

    logo changes

--- client/src/text.js
+++ client/src/text.js
@@ -13,7 +13,7 @@ const TOOLBAR_OPTIONS = [
   [{ color: [] }, { background: [] }],
   [{ script: "sub" }, { script: "super" }],
   [{ align: [] }],
-  ["image", "blockquote", "code-block"],
+  ["blockquote", "code-block"],
   ["clean"],
 ]
 
